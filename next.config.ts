b63e8ae46aea67546a18af34aa01a07d58d6f09a
Ehtesham Ul Haq <ihtishamb7@gmail.com>
2025-09-07 19:49:42 +0500

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverActions: { allowedOrigins: [] },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
